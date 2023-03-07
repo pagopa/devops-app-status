3f72f274a99ccbcea7dfade370b01c07f9c882c0
Diego Lagos <92735530+diegolagospagopa@users.noreply.github.com>
2023-03-07 17:39:27 +0100

    feat: Create simple endpoint for status (#1)
    * deleted old files
    
    * fix docker file and docker-compose
    
    * created status and healt endpoints
    
    * minor fix
    
    * requirements: removed dependencies not more used

--- app/routes/status.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Response
-from http import HTTPStatus
-
-status_route = APIRouter()
-
-
-@status_route.get("/")
-async def status():
-    return Response(status_code=HTTPStatus.OK)
