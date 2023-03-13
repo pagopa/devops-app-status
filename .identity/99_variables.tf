246203bf727bed1442bb8d2cd466a0f8920a7272
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-03-13 23:35:32 +0100

    fix variables

--- .identity/99_variables.tf
+++ .identity/99_variables.tf
@@ -1,12 +1,7 @@
 locals {
   project  = "${var.prefix}-${var.env_short}"
   app_name = "github-${var.github.org}-${var.github.repository}-${var.env}"
-
-  #
-  # Storage state
-  #
-  tfstate_storage_account_name    = "dvopladstinfraterraform"
-  tfstate_storage_account_rg_name = "io-infra-rg"
+  github_env_name = "${var.env}-aks"
 
 
   #
@@ -14,7 +9,6 @@ locals {
   #
   container_app_github_runner_env_name = "dvopla-d-neu-core-github-runner-cae"
   container_app_github_runner_env_rg   = "dvopla-d-neu-core-github-runner-rg"
-
 }
 
 variable "prefix" {
@@ -54,35 +48,4 @@ variable "github_token" {
   type        = string
   sensitive   = true
   description = "GitHub Organization and repository name"
-}
-
-variable "environment_ci_roles" {
-  type = object({
-    subscription = list(string)
-  })
-  description = "GitHub Continous Integration roles"
-}
-
-variable "github_repository_environment_ci" {
-  type = object({
-    protected_branches     = bool
-    custom_branch_policies = bool
-  })
-  description = "GitHub Continous Integration roles"
-}
-
-variable "environment_cd_roles" {
-  type = object({
-    subscription = list(string)
-  })
-  description = "GitHub Continous Delivery roles"
-}
-
-variable "github_repository_environment_cd" {
-  type = object({
-    protected_branches     = bool
-    custom_branch_policies = bool
-    reviewers_teams        = list(string)
-  })
-  description = "GitHub Continous Integration roles"
 }
