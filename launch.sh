392ee93a05f6acb701a14b035b1463a6bef921dc
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-12-22 00:52:07 +0100

    removed python apps

--- launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Launch the application using gunicorn backend
-#gunicorn -w 4 -b 0.0.0.0:${APP_ENDPOINT_PORT:-8045} app.main:app
-# gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:${APP_ENDPOINT_PORT:-8045} --preload --log-level ${VERBOSIT:-INFO} app.main:app
-# python ./app/main.py
-uvicorn app.main:app --host 0.0.0.0 --port ${APP_ENDPOINT_PORT:-8045} --log-level ${APP_VERBOSITY:-info} --reload
